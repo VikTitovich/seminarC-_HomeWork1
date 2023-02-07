600eb1a1e35e903e0d0ecd1df14ff92ad534fc63
Viktor Titovich <raffityinet@gmail.com>
2023-02-07 12:35:33 +0300

    add 3.1

---
+++ dz3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿// *************************************************************************************************************************
+// 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Enter five-digit number: ");
+
+                    bool isnumber = int.TryParse(Console.ReadLine(), out int number);
+
+                    if (isnumber)
+                    {
+                        if ((number >= 10000) && (number < 99999))
+                        {
+                            Console.WriteLine($"The second digit of the number {number} is {(number / 10) % 10}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You didn't enter a three-digit number.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You didn't enter a number.");
+                    }
