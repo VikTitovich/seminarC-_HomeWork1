ffee5cb4438ad6224b5bd414fa7f229fae26fcba
Viktor Titovich <raffityinet@gmail.com>
2023-02-08 08:54:46 +0300

    add code

--- dz3.2/Program.cs
+++ dz3.2/Program.cs
@@ -5,3 +5,13 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+Console.WriteLine("Введите коордитанты точки x1, y1:");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите коордитанты точки x2, y2:");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+Console.WriteLine($"Расстояние между точками равно = {result}");
+
