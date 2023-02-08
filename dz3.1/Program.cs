7313dcefae1444beb70cf5db40bd6b6a3a361cc7
Viktor Titovich <raffityinet@gmail.com>
2023-02-08 16:09:51 +0300

    Add new code

--- dz3.1/Program.cs
+++ dz3.1/Program.cs
@@ -1,4 +1,5 @@
-﻿// *************************************************************************************************************************
+﻿using System.Globalization;
+// *************************************************************************************************************************
 // 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
 // 14212 -> нет
@@ -7,22 +8,35 @@
 
 // 23432 -> да
 
-Console.WriteLine("Enter five-digit number: ");
+// *************************************************************************************************************************
 
-                    bool isnumber = int.TryParse(Console.ReadLine(), out int number);
+Task_1();
 
-                    if (isnumber)
-                    {
-                        if ((number >= 10000) && (number < 99999))
-                        {
-                            Console.WriteLine($"The second digit of the number {number} is {(number / 10) % 10}");
-                        }
-                        else
-                        {
-                            Console.WriteLine("You didn't enter a three-digit number.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("You didn't enter a number.");
-                    }
+void Task_1()
+{
+    Console.WriteLine("Input Number");
+
+    string number = Console.ReadLine();
+
+    if (number.Length == 5)
+    {
+        CheckPalindrom(number);
+    }
+    else
+    {
+        Console.WriteLine($"Error! Enter five-digit number");
+    }
+}
+
+void CheckPalindrom(string number)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"Your Number: {number} - palindrom.");
+    }
+    else
+    {
+        Console.WriteLine($"Your Number: {number} - none palindrom.");
+    }
+}
+
