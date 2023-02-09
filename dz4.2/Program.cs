bad4097b86d3e636d49026ee2afacef97957b78e
Viktor Titovich <raffityinet@gmail.com>
2023-02-09 11:29:51 +0300

    task 4.2 ready

---
+++ dz4.2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine(CalculateSumNumbers(ReadInt("number")));
+
+int ReadInt(string argument)
+{
+    Console.WriteLine($"Input {argument} ");
+    int number;
+
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("It's not a number. Try again.");
+    }
+
+    return number;
+}
+
+int CalculateSumNumbers(int number)
+{
+    int sum = 0;
+    int counter = 0;
+
+    while (number != 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+        counter++;
+    }
+    return sum;
+}
