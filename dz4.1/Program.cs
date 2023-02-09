fde05e0877186eb436b9a839fb20489005d96152
Viktor Titovich <raffityinet@gmail.com>
2023-02-09 09:26:20 +0300

    task ready

--- dz4.1/Program.cs
+++ dz4.1/Program.cs
@@ -1,42 +1,18 @@
-﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную 
+//степень B.
 // 3, 5 -> 243 (3⁵)
-
 // 2, 4 -> 16
+//*********************************************************************************************
 
-// int Exponentiation(int numberA, int numberB){
-//   int result = 1;
-//   for(int i=1; i <= numberB; i++){
-//     result = result * numberA;
-//   }
-//     // int result = Math.Pow(numberA, numberB);
-//     return result;
-// }
+Task_1();
 
-//   Console.Write("Введите число A: ");
-//   int numberA = Convert.ToInt32(Console.ReadLine());
-//   Console.Write("Введите число B: ");
-//   int numberB = Convert.ToInt32(Console.ReadLine());
+void Task_1()
+{
+    int result = Exponent(ReadInt("Enter Number A:"), ReadInt("Enter Degree B:"));
 
-//   int exponentiation = Exponentiation(numberA, numberB);
-//   Console.WriteLine("Ответ: " + exponentiation);
+    Console.WriteLine(result);
 
-
-  ///////////////////////////////////////////////////////////////
-  Task_1();
-
-  void Task_1()
-  {
-     ReadInt("Enter Number A:");
-     int numberA = 0;
-     //int.TryParse(Console.ReadLine(), out int numberA);
-     ReadInt("Enter Degree B:");
-     int numberB = 0;
-     //int.TryParse(Console.ReadLine(), out int numberB);
-    Exponent(numberA, numberB);
-
-  }
-
+}
 
 int ReadInt(string argument)
 {
@@ -51,15 +27,14 @@ int ReadInt(string argument)
     return number;
 }
 
-void Exponent (int numberA, int numberB)
+int Exponent(int numberA, int numberB)
 {
-  int result = 1;
-  for(int i = 1; i <= numberB; i++)
-  {
-    result = result * numberA;
-  }
-    // int result = Math.Pow(numberA, numberB);
-    Console.WriteLine(result);
-    
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = (int)Math.Pow(numberA, numberB);
+
+    }
+    return result;
 }
 
