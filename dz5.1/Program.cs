314383cdca144f8d1850b562c70587bc1d18ef8b
Viktor Titovich <raffityinet@gmail.com>
2023-02-10 16:22:51 +0300

    5.1

---
+++ dz5.1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] array = GetRandomArray(6, 100, 1000);
+
+int count = GetCountEvenNumbers(array);
+
+Console.Clear();
+
+Console.WriteLine($"In Your Array [{string.Join(", ", array)}] : {count} even element(s)");
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int GetCountEvenNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
