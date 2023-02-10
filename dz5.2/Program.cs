335a88ab29be2c4d98e3bc14e008791b12f990b4
Viktor Titovich <raffityinet@gmail.com>
2023-02-10 16:22:33 +0300

    5.2

---
+++ dz5.2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
+//  стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = GetRandomArray(6, 10, 100);
+
+int sum = GetSumOddIndex(array);
+
+Console.WriteLine($"In Your Array [{string.Join(", ", array)}] : sum odd elements = {sum} ");
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int GetSumOddIndex(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
