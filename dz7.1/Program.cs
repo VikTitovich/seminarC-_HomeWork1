0f2d2d9e75b5a3e945537d8b5287fd59038223e4
Viktor Titovich <raffityinet@gmail.com>
2023-02-20 15:56:46 +0300

    dz7.1

---
+++ dz7.1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int m = ReadInt("Enter number m: ");
+
+int n = ReadInt("Enter number n: ");
+
+double[,] array = new double[m, n];
+
+GetRandomArrayDouble(array);
+
+WriteArray(array);
+
+Console.WriteLine();
+
+
+int ReadInt(string argument)   
+{
+    Console.Write(argument);
+
+    int number;
+
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("Error! Try again.");
+    }
+    return number;
+}
+
+void GetRandomArrayDouble (double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+           array[i, j] = new Random().NextDouble() * 20 - 10; 
+        }
+    }
+}
+
+void WriteArray (double[,] array)
+{
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        double alignNumber = Math.Round(array[i, j], 1);
+        Console.Write(alignNumber + " ");
+      }
+      Console.WriteLine();
+  }
+}
+
+
