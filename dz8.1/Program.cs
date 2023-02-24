f44183179ee93a528a06f603f88eea49b9bfa04b
Viktor Titovich <raffityinet@gmail.com>
2023-02-24 11:11:18 +0300

    dz8.1

---
+++ dz8.1/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int numberLines = ReadInt("Enter Number Lines: ");
+
+int numberRows = ReadInt("Enter Number Rows: ");
+
+int[,] array = new int[numberLines, numberRows];
+
+GetRandomArrayInt(array);
+
+PrintArray(array);
+
+Console.WriteLine("\nNew Array:");
+
+OrderArrayLines(array);
+
+PrintArray(array);
+
+int ReadInt(string argument)
+{
+    Console.Write(argument);
+
+    int number;
+
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("Error! Try again.");
+    }
+    return number;
+}
+
+void GetRandomArrayInt(int[,] array)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < numberLines; i++)
+    {
+        for (int j = 0; j < numberRows; j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+
+          array[i, k + 1] = array[i, k];
+
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
